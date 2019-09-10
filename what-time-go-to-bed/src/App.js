90430a019fca193f79d1169fd37275a39f8d1631
billyvollman <b.vollman@gmail.com>
2019-09-10 11:30:09 +1000

    added input fields start location and final location.  Also have the google maps working to figure out duration time based on start and final locations

--- what-time-go-to-bed/src/App.js
+++ what-time-go-to-bed/src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+// import logo from './logo.svg';
+// import './App.css';
+// import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MapContainer extends React.Component {
+  state = {
+    startLocation: '',
+    finalDestination: '',
+    travelTime: 0
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const {startLocation} = this.state
+    const {finalDestination} = this.state
+    console.log(startLocation)
+    console.log(finalDestination)
+    const { google } = this.props
+    const service = new google.maps.DistanceMatrixService()
+      service.getDistanceMatrix(
+        {
+          origins: [startLocation],
+          destinations: [finalDestination],
+          travelMode: 'DRIVING'
+        },
+        (response, status) => {
+          console.log('response', response)
+          console.log('status', status)
+          if (response.rows[0].elements[0].duration.text.split(' ').length > 2 ) {
+            var travelDuration = (Number(response.rows[0].elements[0].duration.text.split(' ')[0]) * 60) + Number(response.rows[0].elements[0].duration.text.split(' ')[2])
+          } else if (response.rows[0].elements[0].status !== "ZERO_RESULTS") {
+            // travelDuration !== null
+            travelDuration = Number(response.rows[0].elements[0].duration.text.split(' ')[0])
+          } else {
+            console.log('error')
+          }
+          
+          this.setState({
+            travelTime: travelDuration
+          })
+        }
+      )
+  }
+
+  handleStartLocationChange = e => {
+    this.setState({
+      startLocation: e.target.value
+    })
+  }
+
+  handleFinalDestinationChange = e => {
+    this.setState({
+      finalDestination: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>What time to go to Bed?</h1>
+        <form onSubmit={this.handleSubmit} action="">
+          <div>Time {this.state.travelTime}</div>
+          <div>Your starting location</div>
+          <input onChange={this.handleStartLocationChange} type="text"/>
+          <div>Your final destination</div>
+          <input onChange={this.handleFinalDestinationChange}  type="text"/>
+          <div><button>Calculate</button></div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCha11ARLYwPGdsg7_RCac7mcfv9a8GwRM'
+})(MapContainer)
