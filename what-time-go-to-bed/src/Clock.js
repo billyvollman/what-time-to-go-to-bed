6087cf6f3534f18671c0765e158ab17610a4ca34
billyvollman <b.vollman@gmail.com>
2019-09-12 12:39:21 +1000

    added css styling to page and current time clock. Also added markup classes to help with current clock component

--- what-time-go-to-bed/src/Clock.js
+++ what-time-go-to-bed/src/Clock.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import moment from 'moment'
+import './Clock.css'
 
 export default class Clock extends React.Component {
 
     state = {
-        currentTime: this.timeNow
+        currentTime: this.timeNow,
+        currentHrs: this.timeNow,
+        currentMins: this.timeNow,
+        currentSecs: this.timeNow
       }
 
     timeNow = () => {
@@ -13,8 +17,14 @@ export default class Clock extends React.Component {
       var secs = moment().seconds()
       console.log(`${hrs}:${mins}:${secs}`)
       var time = `${hrs}:${mins}:${secs}`
+      var timeHrs = `${hrs}`
+      var timeMins = `${mins}`
+      var timeSecs = `${secs}`
       this.setState({
-        currentTime: time
+        currentTime: time,
+        currentHrs: timeHrs,
+        currentMins: timeMins,
+        currentSecs: timeSecs
       })
 
     }
@@ -27,23 +37,50 @@ export default class Clock extends React.Component {
             // debugger
             if (hrs < 10 && mins < 10 && secs < 10) {
                 var time = `0${hrs}:0${mins}:0${secs}`
+                var timeHrs = `0${hrs}`
+                var timeMins = `0${mins}`
+                var timeSecs = `0${secs}`
             } else if (hrs < 10 && mins < 10) {
                 var time = `0${hrs}:0${mins}:${secs}`
+                var timeHrs = `0${hrs}`
+                var timeMins = `0${mins}`
+                var timeSecs = `${secs}`
             } else if (hrs < 10 && secs < 10) {
                 var time = `0${hrs}:${mins}:0${secs}`
+                var timeHrs = `0${hrs}`
+                var timeMins = `${mins}`
+                var timeSecs = `0${secs}`
             } else if (hrs < 10) {
                 var time = `0${hrs}:${mins}:${secs}`
+                var timeHrs = `0${hrs}`
+                var timeMins = `${mins}`
+                var timeSecs = `${secs}`
             } else if (mins < 10 && secs < 10 ) {
                 var time = `${hrs}:0${mins}:0${secs}`
+                var timeHrs = `${hrs}`
+                var timeMins = `0${mins}`
+                var timeSecs = `0${secs}`
             } else if (mins < 10 ) {
                 var time = `${hrs}:0${mins}:${secs}`
+                var timeHrs = `${hrs}`
+                var timeMins = `0${mins}`
+                var timeSecs = `${secs}`
             } else if (secs < 10 ) {
                 var time = `${hrs}:${mins}:0${secs}`
+                var timeHrs = `${hrs}`
+                var timeMins = `${mins}`
+                var timeSecs = `0${secs}`
             }else {
                 var time = `${hrs}:${mins}:${secs}`
+                var timeHrs = `${hrs}`
+                var timeMins = `${mins}`
+                var timeSecs = `${secs}`
             }
             this.setState({
-                currentTime: time
+                currentTime: time,
+                currentHrs: timeHrs,
+                currentMins: timeMins,
+                currentSecs: timeSecs
             })
         }, 1000)
     }
@@ -53,11 +90,42 @@ export default class Clock extends React.Component {
     }
 
       render() {
-        const { currentTime } = this.state
+        const { currentTime, currentHrs, currentMins, currentSecs } = this.state
 
         return(
             <section>
                 <h5>{currentTime}</h5>
+
+                <div className="clock-container">
+                <div className="clock-col">
+                    <p className="clock-day clock-timer">
+                    </p>
+                    <p className="clock-label">
+                    Day
+                    </p>
+                </div>
+                <div className="clock-col">
+                    <p className="clock-hours clock-timer"> {currentHrs}
+                    </p>
+                    <p className="clock-label">
+                    Hours
+                    </p>
+                </div>
+                <div className="clock-col">
+                    <p className="clock-minutes clock-timer"> {currentMins}
+                    </p>
+                    <p className="clock-label">
+                    Minutes
+                    </p>
+                </div>
+                <div className="clock-col">
+                    <p className="clock-seconds clock-timer"> {currentSecs}
+                    </p>
+                    <p className="clock-label">
+                    Seconds
+                    </p>
+                </div>
+                </div>
             </section>
         )
       }
